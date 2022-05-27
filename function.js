f704a365103df52b5cfdaed7c660684c51d5e321
fabiofpires <fabiopires89@outlook.com>
2022-05-26 21:04:14 -0300

    Fazendo o footer e o mapa do site Boreal

--- function.js
+++ function.js
@@ -1,1 +1,10 @@
-scrollReveal().reveal('.section-title');
+function botao(){
+    if (document.querySelector('cor').ClassList.contains('button')){
+        document.querySelector('cor').classList.remove('button'); 
+        document.querySelector('cor').classList.add('rosa');
+    } else{
+        document.querySelector('cor').classList.remove('rosa'); 
+        document.querySelector('cor').classList.add('button')
+
+    }
+}
